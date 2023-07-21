4c4455414e500eb8a531ca89f4e00c26d680ddc4
leon <leon057@126.com>
2023-07-21 13:46:52 +0800

    1

--- hw/200_pts/最长连续交替方波信号.py
+++ hw/200_pts/最长连续交替方波信号.py
@@ -1,20 +1,22 @@
 sig_str = input()
 print(sig_str)
+print(len(sig_str))
 
 def get_max_signal(sig_str): 
     sig_list = []
     found_duplicated = False
-    for i, c in list(sig_str).enumerate():
+    for i in range(len(sig_str)):
+        c = sig_str[i]
         if i == 0: 
             sig_list.append(c)
             continue
-        if i == len(sig_list) - 1: 
-            if sig_list[i-1] == '0': 
-                continue
-            else: 
-                sig_list[-1] = sig_list[-1] + '1' 
-                continue
+        if i == len(sig_str) - 1: 
+            if sig_str[i-1] == '1' and (not found_duplicated):
+                sig_list[-1] = sig_list[-1] + '0' 
+            print("final")
+            continue
             
+        print("序号", i)
         last_c = sig_str[i-1]
         next_c = sig_str[i+1]
         if c == '0':
@@ -24,8 +26,11 @@ def get_max_signal(sig_str):
                 found_duplicated = False
                 sig_list.append(c)
             else :
-                sig_list[-1] =  sig_list[-1] + c
+                if not found_duplicated:
+                    sig_list[-1] =  sig_list[-1] + c
+                
         else:
+            if found_duplicated: continue
             if last_c == '1' or next_c == '1':
                 found_duplicated = True
                 sig_list.pop()
@@ -37,9 +42,9 @@ def get_max_signal(sig_str):
     sig_len_list = [len(sig) for sig in sig_list]
     max = sig_len_list[0]
     max_idx = 0
-    for i, e in sig_len_list.enumerate():
-        if e > max:
-            max = e
+    for i in range(len(sig_len_list)):
+        if sig_len_list[i] > max:
+            max = sig_len_list[i]
             max_idx = i
     return sig_list[max_idx]
     
